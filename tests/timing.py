e536a4ac6908bad5c1192015d6ce63a093e10b28
alberthli <alberthli@berkeley.edu>
2023-06-20 10:01:57 -0700

    try activating persistent pre-compilation on GPU by setting environment var

--- tests/timing.py
+++ tests/timing.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 # import cvxpy as cp
@@ -8,6 +9,8 @@ from jax.experimental.compilation_cache import compilation_cache as cc
 
 from jax_simplex.simplex import linprog
 
+# activating GPU persistent cache compilation on GPU
+os.environ["XLA_FLAGS"] = "--xla_gpu_enable_xla_runtime_executable=true"
 cc.initialize_cache("jax_cache")
 
 """Times the jit-compiled simplex method vs. Gurobi via the cvxpy interface."""
@@ -37,11 +40,11 @@ n = 20
 m = 20
 k = 20
 start = time.time()
-c = jnp.array(np.random.randn((B, n)))
-A_ineq = jnp.array(np.random.randn((B, m, n)))
-b_ineq = jnp.array(np.random.randn((B, m)))
-A_eq = jnp.array(np.random.randn((B, k, n)))
-b_eq = jnp.array(np.random.randn((B, k)))
+c = jnp.array(np.random.randn(B, n))
+A_ineq = jnp.array(np.random.randn(B, m, n))
+b_ineq = jnp.array(np.random.randn(B, m))
+A_eq = jnp.array(np.random.randn(B, k, n))
+b_eq = jnp.array(np.random.randn(B, k))
 batch_linprog(c, A_ineq, b_ineq, A_eq, b_eq)[0].block_until_ready()
 end = time.time()
 print(f"Batch-normalized solve time: {(end - start) / B}")
